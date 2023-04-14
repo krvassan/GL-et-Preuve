01786208a86fe91af0aa81829acc144a9b6e8a89
krvassan <krv.tirou@gmail.com>
2023-04-14 14:03:50 +0200

    Fin ex 5

---
+++ TD-1/DIP/src/main/java/fr/krvassan/dip/UneClasseMetier.java
@@ -0,0 +1,20 @@
+package fr.krvassan.dip;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UneClasseMetier {
+  
+  static final Logger logger = LogManager.getLogger();
+
+  public void uneMethodeMetier() {
+    
+    logger.debug("Début méthode");
+    
+    
+    logger.debug("Fin");
+
+    
+  }
+
+}
